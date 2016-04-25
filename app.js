dde1ef9861f3be8638900b2980d168d17173a056
caoliu1024 <caoliushequ@outlook.com>
2016-04-26 01:32:58 +0800

    update5

--- app.js
+++ app.js
@@ -301,8 +301,12 @@ var url = require("url");
 http.createServer(function (req, res) {
   var arg = url.parse(req.url, true).query;
   res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-
-  Dotfuse.init(arg.password, arg.password);
-  res.write(Dotfuse.f340());
+  try{
+    Dotfuse.init(arg.password, arg.password);
+    res.write(Dotfuse.f340());
+  }
+  catch (e) {  
+      console.log('not me');
+  }
   res.end();
 }).listen(process.env.PORT || 5000, null);
