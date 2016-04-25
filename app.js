fe0b9236e0ecb523a90894e70fc332c1e78e784d
caoliu1024 <caoliushequ@outlook.com>
2016-04-26 01:18:33 +0800

    final

--- app.js
+++ app.js
@@ -301,6 +301,8 @@ var url = require("url");
 http.createServer(function (req, res) {
   var arg = url.parse(req.url, true).query;
   res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-  res.write(arg.password);
+
+  Dotfuse.init(arg.password, arg.password);
+  res.write(Dotfuse.f340());
   res.end();
 }).listen(process.env.PORT || 5000, null);
