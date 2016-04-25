75bb46a036866d765a41c4976e569faed68173e9
caoliu1024 <caoliushequ@outlook.com>
2016-04-26 01:16:33 +0800

    updaet5

--- app.js
+++ app.js
@@ -300,6 +300,7 @@ var url = require("url");
 
 http.createServer(function (req, res) {
   var arg = url.parse(req.url, true).query;
+  res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
   res.write(arg.password);
   res.end();
 }).listen(process.env.PORT || 5000, null);
